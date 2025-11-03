21f57711b5001e0ee40e168a80a4a8a41157c6dc
jowen-jowen <japquinones1977@gmail.com>
2025-11-03 18:01:19 +0800

    Dunkin Donut

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('Home.html')
 
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('Contact.html')
+
+@app.route('/products')
+def products():
+    return render_template('Products.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
